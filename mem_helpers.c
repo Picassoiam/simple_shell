2184ff0c155dfbdfa98f8b6d455beab76e109326
eddie <eddie@yahoo.com>
2023-11-17 12:13:03 -0800

    mem_helpers

---
+++ mem_helpers.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * free_array - free an array of strings
+ * @array: array to free
+ */
+
+void free_array(char **array)
+{
+	size_t i;
+
+	for (i = 0; array[i] != NULL; i++)
+	{
+		free(array[i]);
+	}
+
+	free(array);
+
+}
+
